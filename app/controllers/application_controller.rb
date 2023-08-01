50dbff56a78d644260ce548e3ab025d9efc8f88e
Mohammad Mohsen Haidari <mohsenhaidari0766@gmail.com>
2023-08-01 14:10:46 +0430

    home controller added plus styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
     before_action :authenticate_user!
+    
+  protected
+
+  def authenticate_user_except_home_page
+    return if controller_name == 'home' && action_name == 'index'
+    authenticate_user!
+  end
 end
