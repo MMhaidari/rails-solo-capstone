51f99ae504a42bce7132eeb5081ca908164ae0a3
Mohammad Mohsen Haidari <mohsenhaidari0766@gmail.com>
2023-08-01 14:33:02 +0430

    categories create action added

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,5 +6,26 @@ class CategoriesController < ApplicationController
   def new
     @category = Category.new
   end
+  
+  def create
+    @category = Category.new(category_params)
+    @category.user = current_user
 
+    respond_to do |format|
+      if @category.save
+        format.html { redirect_to categories_path, notice: 'Category created successfully!' }
+        format.json { render json: @category, status: :created }
+      else
+        puts @category.errors.full_messages
+        format.html { render :new, flash: { error: 'Category not created!' } }
+        format.json { render json: @category.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,15 @@ Rails.application.routes.draw do
 
   devise_scope :user do
     authenticated :user do
-      root to: 'users#index', as: :authenticated_root
+      root to: 'categories#index', as: :authenticated_root
     end
 
     unauthenticated :user do
       root to: 'home#index', as: :unauthenticated_root
     end
+  end
+
+    resources :categories, only: %i[new create index] do
+    resources :transaction_entries, only: %i[new create index]
   end
 end
